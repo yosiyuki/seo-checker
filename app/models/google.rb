e2e0c895bf49fca556cf2d1b0371ca7e8568f5e6
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2013-05-03 13:43:27 +0700

    fix logger method in model

--- app/models/google.rb
+++ app/models/google.rb
@@ -20,7 +20,7 @@ class Google
     doc = Nokogiri::HTML(open(url))
     match = doc.xpath("//div[@id='resultStats']").text.match(/([\d,]+)/)
     unless match
-      logger.warn doc.html
+      Rails.logger.warn doc.html
       return false
     end
     match[0].gsub(/,/, "").to_i
