65856e4554e5c49c145b85a4783af44fe268aa23
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2017-03-27 16:25:57 +0700

    remove old notation

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,6 +1,6 @@
 class Keyword < ActiveRecord::Base
   belongs_to :site
-  has_many :keyword_logs, order: "created_at desc"
+  has_many :keyword_logs
 
   def log_at date
     KeywordLog.where(keyword_id: id, created_at: date.beginning_of_day..date.end_of_day).first
