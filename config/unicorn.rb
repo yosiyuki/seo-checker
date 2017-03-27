278bce079f8bb7bc86f4ef6dfd1da446fb253599
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2017-03-27 15:30:32 +0700

    update unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,26 @@
+worker_processes 2
+
+listen "#{RAILS_ROOT}/tmp/sockets/unicorn.sock"
+pid "#{RAILS_ROOT}/tmp/pids/unicorn.sock"
+
+RAILS_ROOT = File.expand_path("../..", __FILE__)
+stderr_path "#{RAILS_ROOT}/log/#{Rails.env}.log"
+stdout_path "#{RAILS_ROOT}/log/#{Rails.env}.log"
+
+preload_app true
+
+before_fork do |server, worker|
+  old_pid = "#{server.config[:pid]}.oldbin"
+    if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      puts "failed kill old process"
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
