a77c9331fdae9334630b0f1411bac913de34b859
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2017-03-27 15:42:24 +0700

    update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,12 @@
 worker_processes 2
 
+RAILS_ROOT = File.expand_path("../..", __FILE__)
+
 listen "#{RAILS_ROOT}/tmp/sockets/unicorn.sock"
 pid "#{RAILS_ROOT}/tmp/pids/unicorn.sock"
 
-RAILS_ROOT = File.expand_path("../..", __FILE__)
-stderr_path "#{RAILS_ROOT}/log/#{Rails.env}.log"
-stdout_path "#{RAILS_ROOT}/log/#{Rails.env}.log"
+stderr_path "#{RAILS_ROOT}/log/unicorn.log"
+stdout_path "#{RAILS_ROOT}/log/unicorn.log"
 
 preload_app true
 
